9f95a41054d4733b9f2d7181b6baa4702dfacc93
Jordan Moossazadeh <jordandmoossazadeh@gmail.com>
2022-04-29 16:55:47 -0700

    fix absolute imports with expo

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,20 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            assets: './assets',
+            components: './components',
+            constants: './constants',
+            hooks: './hooks',
+            screens: './screens',
+          }
+        }
+      ],
+    ],
   };
 };
