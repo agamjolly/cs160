d7b345484a6df68fb8c384339208b38bc60003e2
Jordan Moossazadeh <jordandmoossazadeh@gmail.com>
2022-04-29 18:51:58 -0700

    add property to styled title for middle of screen

--- components/StyledText.tsx
+++ components/StyledText.tsx
@@ -1,9 +1,11 @@
 import { Text, TextProps } from './Themed';
 import styled from 'styled-components/native';
 
-const TitleText = styled(Text)`
+const TitleText = styled(Text) <{ underContent?: boolean }>`
 	fontSize: 50;
 	fontWeight: bold;
+	marginBottom: 30px;
+	${p => p.underContent && `marginTop: 30px;`}
 `;
 
 const HeadingText = styled(Text)`
@@ -11,7 +13,13 @@ const HeadingText = styled(Text)`
 	fontWeight: bold;
 `;
 
-export const Title = (props: TextProps) =>
+type AdditionalTitleProps = {
+	underContent?: boolean;
+};
+
+type TitleProps = AdditionalTitleProps & TextProps;
+
+export const Title = (props: TitleProps) =>
 	<TitleText {...props} />;
 
 export const Heading = (props: TextProps) =>
