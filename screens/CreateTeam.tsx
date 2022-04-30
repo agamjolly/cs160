8ecbc53cf437658243b665cbd4a1a1940e16b51b
Jordan Moossazadeh <jordandmoossazadeh@gmail.com>
2022-04-30 15:26:50 -0700

    create team complete

--- screens/CreateTeam.tsx
+++ screens/CreateTeam.tsx
@@ -1,16 +1,43 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Button } from "react-native";
+import styled from "styled-components/native";
+import { FontAwesome } from "@expo/vector-icons";
+
 import { RootStackScreenProps } from "types";
+import { Text, View } from "components/Themed";
 import ScreenContainer from "components/ScreenContainer";
 import { Title } from 'components/StyledText';
 import TextInput from "components/TextInput";
-import { Button } from "react-native";
-import { useEffect } from "react";
 
 type Member = {
 	name: string;
 	email: string;
 	role: string;
 }
+
+const MembersContainer = styled(View)`
+	display: flex;
+	flex-direction: row;
+	gap: 40px;
+	padding-bottom: 20px;
+	margin-bottom: 30pt;
+	overflow: scroll;
+`;
+
+const MemberInfoContainer = styled(View)`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
+
+const MemberInfo = ({ name, email, role }: Member) => (
+	<MemberInfoContainer>
+		<FontAwesome name="user" size={100} />
+		<Text>{name}</Text>
+		<Text>{email}</Text>
+		<Text>{role}</Text>
+	</MemberInfoContainer>
+);
 
 const TeamCreation = ({ navigation }: RootStackScreenProps<"TeamCreation">) => {
 	const [teamName, setTeamName] = useState("");
@@ -28,6 +55,10 @@ const TeamCreation = ({ navigation }: RootStackScreenProps<"TeamCreation">) => {
 		setMemberRole("")
 	}
 
+	const handleCreateTeam = () => {
+
+	}
+
 	useEffect(() => {
 		console.log(members);
 	}, [members]);
@@ -36,11 +67,14 @@ const TeamCreation = ({ navigation }: RootStackScreenProps<"TeamCreation">) => {
 		<ScreenContainer>
 			<Title>Team Details</Title>
 			<TextInput
+
 				placeholder="Team name"
 				onChangeText={setTeamName}
 			/>
 			<TextInput
 				placeholder="Team description"
+				multiline
+				numberOfLines={5}
 				onChangeText={setTeamDescription}
 			/>
 
@@ -52,6 +86,7 @@ const TeamCreation = ({ navigation }: RootStackScreenProps<"TeamCreation">) => {
 			/>
 			<TextInput
 				textContentType="emailAddress"
+				keyboardType="email-address"
 				placeholder="Member email"
 				value={currMemberEmail}
 				onChangeText={setMemberEmail}
@@ -64,6 +99,29 @@ const TeamCreation = ({ navigation }: RootStackScreenProps<"TeamCreation">) => {
 			<Button
 				title="Add Member"
 				onPress={handleAddMember}
+				disabled={currMemberName === "" || currMemberEmail === "" || currMemberRole === ""}
+			/>
+
+			<Title underContent>Members</Title>
+			<MembersContainer>
+				{members.length > 0 ?
+					members.map((member: Member, i: number) => (
+						<MemberInfo
+							name={member.name}
+							email={member.email}
+							role={member.role}
+							key={`member ${i}`}
+						/>
+					))
+					:
+					<Text>Add members to your team</Text>
+				}
+			</MembersContainer>
+
+			<Button
+				title="Done"
+				onPress={handleCreateTeam}
+				disabled={members.length === 0 || teamName === "" || teamDescription === ""}
 			/>
 		</ScreenContainer>
 	);
