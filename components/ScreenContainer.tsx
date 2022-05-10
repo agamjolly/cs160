a2d79165435fb35709591b6a517b2eb2b6b01dce
Jordan Moossazadeh <jordandmoossazadeh@gmail.com>
2022-05-09 18:25:44 -0700

    update window dimensions on change

--- components/ScreenContainer.tsx
+++ components/ScreenContainer.tsx
@@ -1,14 +1,7 @@
+import { useEffect, useState } from "react";
 import { Dimensions } from "react-native";
 import styled from 'styled-components/native';
 import { View, ViewProps } from "components/Themed";
-
-const { height: scrHeight } = Dimensions.get('window');
-
-const Container = styled(View) <{ scrollable?: boolean; fillHeight?: boolean; tabScreen?: boolean }>`
-	padding: 20px;
-	${p => p.fillHeight && `justify-content: space-between;`}
-	${p => p.scrollable ? `min-height:` : `height:`} ${(p => p.tabScreen ? scrHeight - 112 : scrHeight - 64)}px;
-`;
 
 type ScreenContainerProps = {
 	scrollable?: boolean;
@@ -21,8 +14,28 @@ const ScreenContainer = ({
 	scrollable,
 	fillHeight,
 	tabScreen,
-}: ViewProps & ScreenContainerProps) => (
-	<Container scrollable={scrollable} fillHeight={fillHeight} tabScreen={tabScreen}>{children}</Container>
-);
+}: ViewProps & ScreenContainerProps) => {
+	const [windowDimensions, setWindowDimensions] = useState(Dimensions.get('window'));
+	let scrHeight = windowDimensions.height;
+
+	const Container = styled(View) <{ scrollable?: boolean; fillHeight?: boolean; tabScreen?: boolean }>`
+	padding: 20px;
+	${p => p.fillHeight && `justify-content: space-between;`}
+	${p => p.scrollable ? `min-height:` : `height:`} ${(p => p.tabScreen ? scrHeight - 112 : scrHeight - 64)}px;
+`;
+
+	useEffect(() => {
+		const handleResize = () => {
+			setWindowDimensions(Dimensions.get('window'));
+		}
+
+		window.addEventListener('resize', handleResize);
+		return () => window.removeEventListener('resize', handleResize);
+	}, []);
+
+	return (
+		<Container scrollable={scrollable} fillHeight={fillHeight} tabScreen={tabScreen}>{children}</Container>
+	);
+}
 
 export default ScreenContainer;
